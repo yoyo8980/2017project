20244ca1b3f31af0a85edb70b0d7b5bb4af2bfd1
Daeil Chun <daeil.chun@gmail.com>
2017-11-24 17:08:59 +0900

    강좌개설 단순 입력은 테스트; validation 아직 안되고 입력후 lmxindex로 돌려놨으니 그쪽 sql 문 고쳐주면 좋겠음.

--- LMS/WebContent/openlec.jsp
+++ LMS/WebContent/openlec.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,44 +11,48 @@
 <h1>강좌 개설</h1>
 <form action="openlec.do" method="post">
 	<div>
-		<lable for="lecid">강의코드</lable>
-		<input type="text" name="lecid" id="lecid" value=${requestScope.nextid }>
+		<label for="lecid">강의코드</label>
+		<input type="text" name="lecid" id="lecid" value=${requestScope.nextid } readonly="readonly">
 	</div>
 	<div>
-		<lable for="lecname">강의명</lable>
+		<label for="lecname">강의명</label>
 		<input type="text" name="lecname" id="lecname"/>
 	</div>
 	<div>
-		<lable for="tid">강사코드</lable>
+		<label for="tid">강사코드</label>
 		<select name="tid">
-			<c:forEach items=${teacherid } var="tid">
-				<option>${tid }</option>
+			<c:forEach items="${tlist }" var="tIDlist">
+				<option value=${tIDlist.tid }>${tIDlist.tid }</option>
 			</c:forEach>
 		</select>
-	</div>
+	</div>	
 	<div>
-		<lable for="tname">강사명</lable>
+		<label for="tname">강사명</label>
 		<select name="tname">
-			<c:forEach items=${teachername } var="tname">
-				<option>${tname }</option>
+			<c:forEach items="${tlist }" var="tNAMElist">
+				<option value="${tNAMElist.tname }">${tNAMElist.tname }</option>
 			</c:forEach>
 		</select>
 	</div>			
 	<div>
-		<lable for="room">강의장</lable>
-		<select></select>
+		<label for="room">강의장</label>
+		<select name="rid">
+			<c:forEach items="${room }" var="rid">
+				<option value=${rid }>${rid }</option>
+			</c:forEach>
+		</select>
 	</div>
 	<div>
-		<lable for="starting">개강</lable>
+		<label for="starting">개강</label>
 		<input type="month" name="starting" id="starting">
 	</div>
 	<div>
-		<lable for="ending">종강</lable>
+		<label for="ending">종강</label>
 		<input type="month" name="ending" id="ending">
 	</div>
 	<div>
-		<lable for="intro">강의소개</lable>
-		<input type="text" name="intro" id="intro"/>
+		<label for="intro">강의소개</label>
+		<input type="textarea" name="intro" id="intro"/>
 	</div>
 	<div>
 		<button>전송</button>
--- LMS/src/com/hb/model/OpenLecDao.java
+++ LMS/src/com/hb/model/OpenLecDao.java
@@ -6,42 +6,33 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Map;
 
 import com.hb.util.MyOracle;
-
-//This DAO includes DTO features.
+import com.hb.model.TeacherListDto;
 
 public class OpenLecDao {
-	public int lecid;
-	private String lecname;
-	public ArrayList<Integer> teacherid;
-	public ArrayList<String> teachername;
-	private int roomnum;
-	private Date starting;
-	private Date ending;
-	private String intro;
+	public ArrayList<TeacherListDto> tlist;
+	public ArrayList<Integer> roomlist;
 	
-	Connection conn;
-	PreparedStatement pstmt;
-	ResultSet rs;
+	Connection conn=null;
+	PreparedStatement pstmt=null;
+	ResultSet rs=null;
 
-	public OpenLecDao(){
+	public int nextLecid(){
+		int lecid=0;
 		conn=MyOracle.getConnection();
 		try{
 			String sql="select lecid from (select lecid from lectures order by lecid desc) where rownum = 1";
 			pstmt=conn.prepareStatement(sql);
 			rs=pstmt.executeQuery();
+			
 			if(rs.next()){
 				lecid=rs.getInt("lecid")+1;
-			}				
-			
-			sql="select hrid, hrname from hrlist where team='강사'";
-			pstmt=conn.prepareStatement(sql);
-			rs=pstmt.executeQuery();
-			while(rs.next()){
-				teacherid.add(rs.getInt("hrid"));
-				teachername.add(rs.getString("hrname"));
 			}
+			
+			if(rs!=null)rs.close();
+			if(pstmt!=null)pstmt.close();
 			
 		}catch(Exception e){
 		}finally{
@@ -53,15 +44,101 @@ public class OpenLecDao {
 				e.printStackTrace();
 			}
 		}
-	}
+		return lecid;
+	} //method end
 	
-	OpenLecDao(String[] formvalues){
+	public ArrayList<TeacherListDto> teacherList(){
+		conn=MyOracle.getConnection();
+		String sql="select hrid, hrname from hrlist where team='teach'";
+		tlist=new ArrayList<TeacherListDto>();
+		try {
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+				TeacherListDto bean=new TeacherListDto();
+				bean.setTid(rs.getInt("hrid"));
+				bean.setTname(rs.getString("hrname"));
+				tlist.add(bean);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return tlist;
+	} //method end
+	
+	public ArrayList<Integer> roomList(){
+		conn=MyOracle.getConnection();
+		String sql="select rid from room";
+		roomlist = new ArrayList<Integer>();
 		
-	}
+		try {
+			pstmt=conn.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+				roomlist.add(rs.getInt("rid"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return roomlist;
+	} //method end
 	
-	public void pushForm(){
-		//위 생성자로 세팅된 값을 DB로 입력후 
-		//다시 controller 로 보내서 처리하되 코드를 줘서 같은 jsp 
-	}
+	
+	public void pushForm(Map paramMap){
+		int lecid = Integer.parseInt((String) paramMap.get("lecid"));
+		String lecname=(String)paramMap.get("lecname");
+		int tid=Integer.parseInt((String)paramMap.get("tid"));
+		int rid=Integer.parseInt((String)paramMap.get("rid"));
+		String tname=(String)paramMap.get("tname");
+		String starting=(String)paramMap.get("starting");
+		String ending=(String)paramMap.get("ending");
+		String intro=(String)paramMap.get("intro");
+		
+		String sql="insert into lectures values (?,?,?,?,?,to_date(?, 'yyyy-mm'),to_date(?, 'yyyy-mm'),?,?)";
+		conn=MyOracle.getConnection();
+		
+		try {
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setInt(1, lecid);
+			pstmt.setString(2, lecname);
+			pstmt.setInt(3, tid);
+			pstmt.setInt(4, rid);
+			pstmt.setString(5, tname);
+			pstmt.setString(6, starting);
+			pstmt.setString(7, ending);
+			pstmt.setString(8, intro);
+			pstmt.setString(9, "waiting");
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	} //method end
 	
 }
