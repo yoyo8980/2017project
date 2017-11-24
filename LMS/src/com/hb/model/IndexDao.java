e62b6b98513ae08e6087809f691e58cce4aac92b
miran <k1025mr@gmail.com>
2017-11-24 09:10:50 +0900

    indexcontroller utf-8 수정

--- LMS/src/com/hb/model/IndexDao.java
+++ LMS/src/com/hb/model/IndexDao.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import com.hb.model.index.IndexDto;
 import com.hb.util.MyOracle;
 
 
@@ -19,7 +20,7 @@ public class IndexDao {
 	
 	ArrayList<IndexDto> list;
 	public ArrayList<IndexDto> indexView(){
-		String sql="select lecid,lecname from lectures where status='운영중'";
+		String sql="select lecid,lecname from lectures where status='수업중'";
 		String sql2="select count(sid) as cnt from stu where regclass = ? ";
 		conn=MyOracle.getConnection();
 		try{
@@ -41,7 +42,6 @@ public class IndexDao {
 				bean.setSid(rs2.getInt("cnt"));
 				list.add(bean);		
 			}				
-			System.out.println("데이터 받아 옴");
 			
 		}catch(Exception e){
 		}finally{
